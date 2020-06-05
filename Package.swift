344c64b6ff40b9cf862e14db5001aa46def67c29
Felix Herrmann <felix.herrmann@icloud.com>
2020-06-05 07:44:42 +0200

    Package: platforms iOS VersionString to 9.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FHConstraints",
     platforms: [
-        .iOS("13.0"),
+        .iOS("9.0"),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
