96b9eda468804515f826dd67a7595a88f2368872
Felix Herrmann <felix.herrmann@icloud.com>
2020-12-05 00:31:42 +0100

    layout guide test

--- Tests/FHConstraintsTests/FHConstraintsTests.swift
+++ Tests/FHConstraintsTests/FHConstraintsTests.swift
@@ -7,25 +7,48 @@ import AppKit
 #endif
 
 final class FHConstraintsTests: XCTestCase {
-    func testExample() {
+    func testView() {
         #if canImport(UIKit)
         let view = UIView()
         let subview = UIView()
         
         view.addSubview(subview)
-        XCTAssertNotNil(subview.constraint(.leading(to: view.leadingAnchor)), "Constraint leading constraint successfully.")
+        
+        XCTAssertNotNil(subview.constraint(.leading(to: view.leadingAnchor)), "Constraint view's leading constraint successfully.")
         
         #elseif canImport(AppKit)
         let view = NSView()
         let subview = NSView()
         
         view.addSubview(subview)
-        XCTAssertNotNil(subview.constraint(.leading(to: view.leadingAnchor)), "Constraint leading constraint successfully.")
+        
+        XCTAssertNotNil(subview.constraint(.leading(to: view.leadingAnchor)), "Constraint view's leading constraint successfully.")
+        
+        #endif
+    }
+    
+    func testLayoutGuide() {
+        #if canImport(UIKit)
+        let view = UIView()
+        let layoutGuide = UILayoutGuide()
+        
+        view.addLayoutGuide(layoutGuide)
+        
+        XCTAssertNotNil(layoutGuide.constraint(.leading(to: view.leadingAnchor)), "Constraint layoutGuide's leading constraint successfully.")
+        
+        #elseif canImport(AppKit)
+        let view = NSView()
+        let layoutGuide = NSLayoutGuide()
+        
+        view.addLayoutGuide(layoutGuide)
+        
+        XCTAssertNotNil(layoutGuide.constraint(.leading(to: view.leadingAnchor)), "Constraint layoutGuide's leading constraint successfully.")
         
         #endif
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testView", testView),
+        ("testLayoutGuide", testLayoutGuide)
     ]
 }
